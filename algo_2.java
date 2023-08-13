351c95342361b3d8b3e942a9895faea99835c375
Kalana Mihiranga <kalanamihiranga97@gmail.com>
2023-08-13 09:10:26 +0530

    Third commit

---
+++ algo_2.java
@@ -0,0 +1,19 @@
+public class algo_2 {
+    public static void main(String[] args) {
+        int[] nums = {-5, 2, 7, 10, 58, -7, 8, 23} ;
+        int x=nums[0];
+        int y=nums[0];
+        
+        for (int i=0;i<nums.length-1;i++){
+     
+            if (x<nums[i+1]){
+                x=nums[i+1];       
+            }
+            if (y>nums[i+1]){
+                y=nums[i+1];   
+            }
+        }
+         System.out.println("LARGEST NUMBER OF THE SET: "+x+"            SMALLEST NUMBER OF THE SET "+y);
+    }
+    
+}
